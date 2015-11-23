8d52570f3b1e3430cc1fdc8c933d353752f9ef3a
hayley <hayley@drumlithie.com>
2015-11-23 13:54:40 +0000

    nope

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 /**
  * Created by 1300777 on 23/11/2015.
  */
-var http = require('http')
+var http = require('http');
 var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
+
 
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
