d82719e0f42269b6a1ba58b4dd30a5e5027938f1
hayley <hayley@drumlithie.com>
2015-11-30 14:19:55 +0000

    nope

---
+++ twittertest.js
@@ -0,0 +1,16 @@
+/**
+ * Created by 1300777 on 30/11/2015.
+ */
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: 'jAvm0dQ55CYyPnku1vTDWCIEg',
+    consumer_secret: 'V8CdE8EnwrxjB2R7M9aPZslAWMhgd4RCW0uYdaJEr4EWWO6p1K',
+    access_token_key: '3230306397-WwdtAd1Zx9KWD6KhdP3vZ3uPYAWXVFm2Stq5ycn',
+    access_token_secret: 'kmVeJ1STFG782ewXNYPzlDyBCLhIaCg0EcYGiE9za79uc'
+});
+
+client.get('search/tweets', {q: 'lolcat'}, function(error, tweets){
+    console.log(tweets);
+});
