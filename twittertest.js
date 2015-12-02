668763de08a5dffeec0cdd3ac72462d1c6a98cc0
hayley <hayley@drumlithie.com>
2015-12-02 16:13:06 +0000

    nope

--- twittertest.js
+++ twittertest.js
@@ -1,6 +1,4 @@
 var Twitter = require('twitter');
-var http = require('http');
-var port = process.env.PORT || 1337;
 
 var client = new Twitter({
     consumer_key: 'jAvm0dQ55CYyPnku1vTDWCIEg',
@@ -9,21 +7,6 @@ var client = new Twitter({
     access_token_secret: 'kmVeJ1STFG782ewXNYPzlDyBCLhIaCg0EcYGiE9za79uc'
 });
 
-http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    client.get('search/tweets', {q: 'lolcats'}, function(error, tweets){
-        response.end(JSON.stringify(tweets));
-    });
-}).listen(port);
-
-
-var json = [];
-
-for (var i =0; i< tweets.statuses.length ; i++)
-{
-
-    json.push({name: tweets.statuses[i].user.name, text: tweets.statuses[i].text});
-}
-
-
-response.end(JSON.stringify(json));
+client.get('search/tweets', {q: 'lolcats'}, function(error, tweets){
+    console.log(tweets);
+});
