c499501f69800933d5650bcd0a7508221f6807d3
hayley <hayley@drumlithie.com>
2015-12-02 16:28:50 +0000

    nope

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 
 var client = new Twitter({
-    consumer_key: 'jAvm0dQ55CYyPnku1vTDWCIEg ',
-    consumer_secret: 'V8CdE8EnwrxjB2R7M9aPZslAWMhgd4RCW0uYdaJEr4EWWO6p1K',
-    access_token_key: '3230306397-WwdtAd1Zx9KWD6KhdP3vZ3uPYAWXVFm2Stq5ycn',
-    access_token_secret: 'kmVeJ1STFG782ewXNYPzlDyBCLhIaCg0EcYGiE9za79uc'
+    consumer_key: 'uXl8060AfB6TtVZOjUPbPqiuU',
+    consumer_secret: 'qWYtc0c6wskWp9OvP63LcFTxnGexfhCjLAfN7gKgcwW7zfcSdv',
+    access_token_key: '14812487-6rIZpOfDmMBGFybfd26Cpe86kGw64MLGaZLaimuFN',
+    access_token_secret: 'efrJVLugBwFeAVnWSx1Cx6Z8N8IJlSB3js7XjW4KUYiuc'
 });
+
 
 
 http.createServer(function(request, response) {
